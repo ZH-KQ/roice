8df868f1c9863e96975deeace766bc019aca34aa
iwadjiwad <zhexploitingpc@outlook.com>
2025-05-03 17:53:31 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -2750,7 +2750,7 @@ do
 	});
 
 	local WatermarkLabel = Library:CreateLabel({
-		Name = "InnerFrameLabel"
+		Name = "InnerFrameLabel";
 		Position = UDim2.new(0, 5, 0, 0);
 		Size = UDim2.new(1, -4, 1, 0);
 		TextSize = 14;
